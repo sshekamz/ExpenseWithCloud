e88b0bb2f15bddc289b8b459680d67cce3c05e49
sshekamz <shashanknkework@gmail.com>
2022-11-30 17:18:38 +0530

    .env File ready

--- util/database.js
+++ util/database.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const sequelize = new Sequelize('expense-track', 'root', 'sshekamz', {
+const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost'
+    host: process.env.DB_HOST
 })
 
 module.exports = sequelize;
